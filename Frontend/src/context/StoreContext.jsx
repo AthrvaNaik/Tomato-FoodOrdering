e00e0a5a63df694d101802a06cb7e7e9a57f86c3
Athrva D Naik <141731433+AthrvaNaik@users.noreply.github.com>
2024-09-16 14:01:55 +0530

    Update StoreContext.jsx

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider = (props)=>{
 
 
     const [cartItems,setCartItems]=useState({});
-    const url = "http://localhost:4000"
+    const url = "https://tomato-foodordering-backend.onrender.com"
 
     const [token,setToken] = useState("")
 
