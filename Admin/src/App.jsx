07db9e0b3a59a133e6ad2f2b46efcb6a138de663
Athrva D Naik <141731433+AthrvaNaik@users.noreply.github.com>
2024-09-16 14:14:57 +0530

    Update App.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
 
 
-  const url = "http://localhost:4000"
+  const url = "https://tomato-foodordering-backend.onrender.com"
 
   return (
     <div>
